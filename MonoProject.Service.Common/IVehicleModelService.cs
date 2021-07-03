6f8abd6da72b576a910a2a409023693bef378e8a
Filip Brkić <filipbrkic5@gmail.com>
2021-07-03 14:56:33 +0200

    added service blueprint

---
+++ MonoProject.Service.Common/IVehicleModelService.cs
@@ -0,0 +1,19 @@
+﻿using MonoProject.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoProject.Service.Common
+{
+    public interface IVehicleModelService
+    {
+        Task<IEnumerable<VehicleModelDTO>> GetAllAsync(ISorting sorting, IFiltering filtering, IPaging paging);
+        Task<int> AddAsync(RegistrationDTO entity);
+        Task<RegistrationDTO> GetAsync(Guid id);
+        Task<int> UpdateAsync(RegistrationDTO entity);
+        Task<int> DeleteAsync(Guid id);
+        Task<int> DeleteAsync(RegistrationDTO entity);
+    }
+}
