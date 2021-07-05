94d7706bf4c9dcf92f59e2797c08ea60b5c73905
Filip Brkić <filipbrkic5@gmail.com>
2021-07-05 17:26:49 +0200

    fixed error in IGenericRepos and added VehicleMakeController blueprint

---
+++ MonoProject.MVC/Controllers/VehicleMakeController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonoProject.MVC.Controllers
+{
+    public class VehicleMakeController : Controller
+    {
+        // GET: VehicleMakeController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: VehicleMakeController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VehicleMakeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VehicleMakeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VehicleMakeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VehicleMakeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VehicleMakeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VehicleMakeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- MonoProject.Repository.Common/IGenericRepository.cs
+++ MonoProject.Repository.Common/IGenericRepository.cs
@@ -9,7 +9,7 @@ namespace MonoProject.Repository.Common
 {
     public interface IGenericRepository
     {
-        Task<(IEnumerable<T>, int)> GetAllAsync<T>(Expression<Func<T, bool>> match, Expression<Func<T, string>>) orderByExpression, int take, int skip, string sortType) where T : class;
+        Task<(IEnumerable<T>, int)> GetAllAsync<T>(Expression<Func<T, bool>> match, Expression<Func<T, string>> orderByExpression, int take, int skip, string sortType) where T : class;
         Task<int> AddAsync<T>(T entity) where T : class;
         Task<T> GetAsync<T>(Guid id) where T : class;
         Task<T> GetUserAsync<T>(Guid id) where T : class;
