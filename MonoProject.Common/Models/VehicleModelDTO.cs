4de3e693181cf662af395135f391e29e76adf5bb
Filip Brkić <filipbrkic5@gmail.com>
2021-07-09 15:50:49 +0200

    added MakeName, removed usings and projectreference

--- MonoProject.Common/Models/VehicleModelDTO.cs
+++ MonoProject.Common/Models/VehicleModelDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MonoProject.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace MonoProject.Common.Models
     {
         public Guid Id { get; set; }
         public Guid MakeId { get; set; }
+        public string MakeName { get; set; }
         public string Name { get; set; }
         public string Abrv { get; set; }
     }
--- MonoProject.MVC/Models/VehicleModelViewModel.cs
+++ MonoProject.MVC/Models/VehicleModelViewModel.cs
@@ -9,6 +9,7 @@ namespace MonoProject.MVC.Models
     {
         public Guid Id { get; set; }
         public Guid MakeId { get; set; }
+        public string MakeName { get; set; }
         public string Name { get; set; }
         public string Abrv { get; set; }
     }
