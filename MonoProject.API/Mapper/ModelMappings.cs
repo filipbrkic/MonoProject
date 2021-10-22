a03fe2f2405d9c22c9e5c5f2122a5885f3ce079a
Filip Brkić <filipbrkic5@gmail.com>
2021-10-22 12:05:58 +0200

    added mappings and created VehicleParams

---
+++ MonoProject.API/Mapper/ModelMappings.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MonoProject.API.Models;
+using MonoProject.Common.Interface;
+using MonoProject.Common.Models;
+using MonoProject.DAL.Models;
+
+namespace MonoProject.API.Mapper
+{
+    public class ModelMappings : Profile
+    {
+        public ModelMappings()
+        {
+            CreateMap<VehicleMake, VehicleMakeDTO>().ReverseMap();
+            CreateMap<VehicleModel, VehicleModelDTO>().ReverseMap();
+
+            CreateMap<VehicleMake, VehicleParams>().ReverseMap();
+            CreateMap<VehicleModel, VehicleParams>().ReverseMap();
+
+            CreateMap<VehicleMakeDTO, VehicleParams>().ReverseMap();
+            CreateMap<VehicleModelDTO, VehicleParams>().ReverseMap();
+
+            CreateMap<Filtering, IFiltering>().ReverseMap();
+            CreateMap<Paging, IPaging>().ReverseMap();
+            CreateMap<Sorting, ISorting>().ReverseMap();
+        }
+    }
+}
---
+++ MonoProject.API/Models/VehicleParams.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MonoProject.API.Models
+{
+    public class VehicleParams
+    {
+        public Guid Id { get; set; }
+        public Guid VehicleMakeId { get; set; }
+        public string Name { get; set; }
+        public string Abrv { get; set; }
+        public string sortOrder { get; set; }
+        public string sortyBy { get; set; }
+        public string searchBy { get; set; }
+        public string search { get; set; }
+        public int? pageNumber { get; set; }
+        public int? pageSize { get; set; }
+    }
+}
