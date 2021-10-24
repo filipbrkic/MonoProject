06d2f05ab4954b41815ccf673abb156e4ab1f570
Filip Brkić <filipbrkic5@gmail.com>
2021-10-24 11:17:58 +0200

    added more mappings

--- MonoProject.API/Mapper/ModelMappings.cs
+++ MonoProject.API/Mapper/ModelMappings.cs
@@ -12,12 +12,21 @@ namespace MonoProject.API.Mapper
         {
             CreateMap<VehicleMake, VehicleMakeDTO>().ReverseMap();
             CreateMap<VehicleModel, VehicleModelDTO>().ReverseMap();
+            CreateMap<VehicleEngineType, VehicleEngineTypeDTO>().ReverseMap();
+            CreateMap<VehicleOwner, VehicleOwnerDTO>().ReverseMap();
+            CreateMap<VehicleRegistration, VehicleRegistrationDTO>().ReverseMap();
 
             CreateMap<VehicleMake, VehicleParams>().ReverseMap();
             CreateMap<VehicleModel, VehicleParams>().ReverseMap();
+            CreateMap<VehicleEngineType, VehicleParams>().ReverseMap();
+            CreateMap<VehicleOwner, VehicleParams>().ReverseMap();
+            CreateMap<VehicleRegistration, VehicleParams>().ReverseMap();
 
             CreateMap<VehicleMakeDTO, VehicleParams>().ReverseMap();
             CreateMap<VehicleModelDTO, VehicleParams>().ReverseMap();
+            CreateMap<VehicleEngineTypeDTO, VehicleParams>().ReverseMap();
+            CreateMap<VehicleOwnerDTO, VehicleParams>().ReverseMap();
+            CreateMap<VehicleRegistrationDTO, VehicleParams>().ReverseMap();
 
             CreateMap<Filtering, IFiltering>().ReverseMap();
             CreateMap<Paging, IPaging>().ReverseMap();
