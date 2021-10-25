45ca92eae4187a70792a6acf9557be3e341a74c2
Filip Brkić <filipbrkic5@gmail.com>
2021-10-25 14:32:12 +0200

    added Model and Owner controllers

---
+++ MonoProject.API/Controllers/VehicleModelController.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MonoProject.API.Models;
+using MonoProject.Common.Models;
+using MonoProject.DAL.Data;
+using MonoProject.Service.Common;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MonoProject.API.Controllers
+{
+    [ApiController]
+    [Route("api/vehiclemodel")]
+    public class VehicleModelController : ControllerBase
+    {
+        private readonly IVehicleModelService vehicleModelService;
+        private readonly IMapper mapper;
+        private readonly IVehicleMakeService vehicleMakeService;
+        private readonly VehicleDbContext context;
+
+        public VehicleModelController(IVehicleModelService vehicleModelService, IMapper mapper, IVehicleMakeService vehicleMakeService, VehicleDbContext context)
+        {
+            this.vehicleModelService = vehicleModelService;
+            this.mapper = mapper;
+            this.vehicleMakeService = vehicleMakeService;
+            this.context = context;
+        }
+        [HttpGet("{Id}", Name = "GetVehicleModel")]
+        public async Task<IActionResult> GetVehicleModel(Guid id)
+        {
+            var vehicleModel = await vehicleModelService.GetAsync(id);
+
+            if (vehicleModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mapper.Map<VehicleModelDTO>(vehicleModel));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllVehicleModel([FromQuery] VehicleParams vehicleParams)
+        {
+            var filtering = new Filtering(vehicleParams.SearchBy, vehicleParams.Search);
+            var paging = new Paging(vehicleParams.PageNumber, vehicleParams.PageSize);
+            var sorting = new Sorting(vehicleParams.SortOrder, vehicleParams.SortyBy);
+
+
+            var vehicleModel = await vehicleModelService.GetAllAsync(filtering, paging, sorting);
+            return Ok(JsonSerializer.Serialize(vehicleModel));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VehicleModelDTO>> PostVehicleModel([FromQuery] VehicleParams vehicleParams)
+        {
+            var vehicleModel = mapper.Map<VehicleModelDTO>(vehicleParams);
+            vehicleModel.MakeId = vehicleParams.VehicleMakeId;
+            await vehicleModelService.AddAsync(vehicleModel);
+
+            return CreatedAtRoute("GetVehicleMake", new { id = vehicleModel.Id }, vehicleModel);
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> UpdateVehicleModel(Guid id)
+        {
+
+            var vehicleGet = await vehicleModelService.GetAsync(id);
+            context.ChangeTracker.Clear();
+
+            if (vehicleGet == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleModel = mapper.Map<VehicleModelDTO>(vehicleGet);
+
+            await vehicleModelService.UpdateAsync(vehicleModel);
+
+
+            return NoContent();
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult> DeleteVehicleModel(Guid id)
+        {
+            await vehicleModelService.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+
+}
---
+++ MonoProject.API/Controllers/VehicleOwnerController.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MonoProject.API.Models;
+using MonoProject.Common.Models;
+using MonoProject.DAL.Data;
+using MonoProject.Service.Common;
+using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MonoProject.API.Controllers
+{
+    [ApiController]
+    [Route("api/vehicleowner")]
+    public class VehicleOwnerController : ControllerBase
+    {
+        private readonly IVehicleOwnerService vehicleOwnerService;
+        private readonly IMapper mapper;
+        private readonly VehicleDbContext context;
+
+        public VehicleOwnerController(IVehicleOwnerService vehicleOwnerService, IMapper mapper, VehicleDbContext context)
+        {
+            this.vehicleOwnerService = vehicleOwnerService;
+            this.mapper = mapper;
+            this.context = context;
+        }
+
+        [HttpGet("{Id}", Name = "GetVehicleOwner")]
+        public async Task<IActionResult> GetVehicleOwner(Guid id)
+        {
+            var vehicleOwner = await vehicleOwnerService.GetAsync(id);
+
+            if (vehicleOwner == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mapper.Map<VehicleOwnerDTO>(vehicleOwner));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllVehicleOwner([FromQuery] VehicleParams vehicleParams)
+        {
+            var filtering = new Filtering(vehicleParams.SearchBy, vehicleParams.Search);
+            var paging = new Paging(vehicleParams.PageNumber, vehicleParams.PageSize);
+            var sorting = new Sorting(vehicleParams.SortOrder, vehicleParams.SortyBy);
+
+
+            var vehicleOwner = await vehicleOwnerService.GetAllAsync(filtering, paging, sorting);
+            return Ok(JsonSerializer.Serialize(vehicleOwner));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VehicleOwnerDTO>> PostVehicleOwner([FromQuery] VehicleParams vehicleParams)
+        {
+            vehicleParams.Id = Guid.NewGuid();
+            var vehicleLink = mapper.Map<VehicleModelToVehicleOwnerLinkDTO>(vehicleParams);
+            var vehicleOwner = mapper.Map<VehicleOwnerDTO>(vehicleParams);
+            vehicleOwner.Id = vehicleLink.ModelId;
+            await vehicleOwnerService.AddAsync(vehicleOwner, vehicleLink);
+
+            return CreatedAtRoute("GetVehicleOwner", new { id = vehicleOwner.Id, vehicleLink.ModelId }, vehicleOwner);
+        }
+
+        [HttpPut("{Id}")]
+        public async Task<IActionResult> UpdateVehicleOwner(Guid id)
+        {
+
+            var vehicleGet = await vehicleOwnerService.GetAsync(id);
+            context.ChangeTracker.Clear();
+
+            if (vehicleGet == null)
+            {
+                return NotFound();
+            }
+
+            var vehicleOwner = mapper.Map<VehicleOwnerDTO>(vehicleGet);
+
+            await vehicleOwnerService.UpdateAsync(vehicleOwner);
+
+
+            return NoContent();
+        }
+
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult> DeleteVehicleOwner(Guid id)
+        {
+            await vehicleOwnerService.DeleteAsync(id);
+
+            return NoContent();
+        }
+    }
+}
