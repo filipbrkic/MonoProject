8f61a0b80bd3ea258807b550883dfcc5b6a6521d
Filip Brkić <filipbrkic5@gmail.com>
2021-11-08 00:17:56 +0100

    updated ownerparams

--- MonoProject.API/Models/OwnerParams.cs
+++ MonoProject.API/Models/OwnerParams.cs
@@ -5,15 +5,11 @@ namespace MonoProject.API.Models
     public class OwnerParams
     {
         public Guid Id { get; set; }
-        public Guid OwnerId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string SortOrder { get; set; }
-        public string SortyBy { get; set; }
-        public string SearchBy { get; set; }
-        public string Search { get; set; }
-        public int? PageNumber { get; set; }
-        public int? PageSize { get; set; }
+
+        public Guid ModelId { get; set; }
+        public string RegistrationNumber { get; set; }
     }
 }
